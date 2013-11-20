86844505fcfac9974c3763c59d13955cde45e44e
tiago <tiago@ubuntu.(none)>
2013-11-20 05:06:49 -0200

    Alterado Delete de produtos para modal

--- produtos/urls.py
+++ produtos/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
      url(r'^$', views.ProdutosList.as_view(), name='produtos_list'),
      url(r'^new$', views.ProdutoCreate.as_view(), name='produto_new'),
      url(r'^edit/(?P<pk>\d+)$', views.ProdutoUpdate.as_view(), name='produto_edit'),
-     url(r'^delete/(?P<pk>\d+)$', views.ProdutoDelete.as_view(), name='produto_delete'),
+     url(r'^delete$', views.ProdutoDelete, name='produto_delete'),
 )
--- produtos/views.py
+++ produtos/views.py
@@ -2,30 +2,15 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView,ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
-
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from models import Produtos
 from forms import ProdutosForms
 
 
-class ProdutosList(ListView, request):
-    
-    produtos_list = Produtos.objects.all()
-    paginator = Paginator(produtos_list, 5)   
-    page = request.GET.get('page')
-    try:
-        produtos = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        produtos = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        produtos = paginator.page(paginator.num_pages)
-
-
-    model = produtos
-
+class ProdutosList(ListView):
+    model = Produtos
  
 class ProdutoCreate(CreateView):
     form_class = ProdutosForms 
@@ -37,8 +22,16 @@ class ProdutoUpdate(UpdateView):
     model = Produtos 
     success_url = reverse_lazy('produtos_list')
 
-class ProdutoDelete(DeleteView):
-    model = Produtos 
-    success_url = reverse_lazy('produtos_list')
+def ProdutoDelete(request):
+    if request.method=='POST':
+       id_prod =  request.POST.get("id_prod")  
+       produto = get_object_or_404(Produtos, pk=id_prod)    
+       produto.delete()
+
+    return redirect('produtos_list')
+
+#class ProdutoDelete(DeleteView):
+#    model = Produtos 
+#    success_url = reverse_lazy('produtos_list')
 
     
